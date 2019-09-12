fcc83b1b9d2f39c9f7cdda31630423deb09a5d1b
weiyd <yitakabe@gmail.com>
2019-09-12 09:28:09 +0800

    ""

---
+++ src/main/java/com/lovefit/App.java
@@ -0,0 +1,13 @@
+package com.lovefit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
